13224b587b9a1ccdff4752a2ee4e271573c18249
KirillØleynik <kirill.olejnik@gmail.com>
2020-09-17 15:30:11 +0300

    Add user authentication command. Validates credentials and returns auth token pair if succed

---
+++ app/commands/authenticate_user.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# Wraps  user authentication logic
+class AuthenticateUser
+  prepend SimpleCommand
+
+  EXPIRE = {
+    access: 5.minutes.from_now.strftime('%Y/%m/%d %H:%M:%S'),
+    refresh: 1.month.from_now.strftime('%Y/%m/%d %H:%M:%S')
+  }.freeze
+
+  def initialize(email:, password:)
+    @email = email
+    @password = password
+  end
+
+  def call
+    return auth_tokens if user
+
+    nil
+  end
+
+  private
+
+  attr_reader :email, :password
+
+  def auth_tokens
+    {
+      access: { token: access_token, expire: EXPIRE[:access] },
+      refresh: { token: refresh_token, expire: EXPIRE[:refresh] }
+    }
+  end
+
+  def access_token
+    JsonWebToken.encode(payload, EXPIRE[:access])
+  end
+
+  def refresh_token
+    JsonWebToken.encode(payload, EXPIRE[:refresh])
+  end
+
+  def payload
+    return {} unless user
+
+    { user_id: user.id }
+  end
+
+  def user
+    user = User.find_by(email: email)
+    return user if user&.authenticate(password)
+
+    errors.add :user_authentication, 'Invalid credentials'
+    nil
+  end
+end
