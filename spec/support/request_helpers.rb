53427514b569720466d6ba1d2e20131bd1b20959
KirillØleynik <kirill.olejnik@gmail.com>
2020-10-08 18:58:09 +0300

    update authstub helper method

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -6,9 +6,9 @@ module Requests
       JSON.parse(response.body)
     end
 
-    def stub_authorization
+    def stub_authorization(user = create(:user))
       auth_result = double('auth_rommand_result')
-      allow(auth_result).to receive(:result).and_return(create(:user))
+      allow(auth_result).to receive(:result).and_return(user)
       allow(AuthorizeRequest).to receive(:call).and_return(auth_result)
     end
   end
