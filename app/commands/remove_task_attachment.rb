f47e7d0fa07d3467ab7900217d77700cc580c3ef
KirillØleynik <kirill.olejnik@gmail.com>
2020-12-05 20:33:27 +0200

    Add task attachment deletion command

---
+++ app/commands/remove_task_attachment.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Wraps task attachment deletion algorithm
+class RemoveTaskAttachment
+  prepend SimpleCommand
+  def initialize(user, task_id)
+    @user = user
+    @task_id = task_id
+  end
+
+  def call
+    return unless [task, user_task_owner?].all?
+
+    task.attachment&.remove!
+    task
+  end
+
+  private
+
+  attr_reader :user, :task_id
+  def task
+    @task ||= Task.find(task_id)
+  rescue ActiveRecord::RecordNotFound
+    errors.add(:task, 'NotFound')
+  end
+
+  def user_task_owner?
+    if user.tasks.include?(task)
+      true
+    else
+      errors.add(:user, 'NotAuthorized')
+      false
+    end
+  end
+end
